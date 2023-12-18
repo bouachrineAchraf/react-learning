0f1f1cfca5cf224fcb1a32d0186ef4357608d323
bouachrine <bouachrineachraf04@gmail.com>
2023-12-18 16:19:40 +0100

    display a list with map

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 
 function App() {
-  const age = 19;
+  const age = 17;
+  const listofname = ["achraf", "oum", "test"]
 
   return (
-    <div>{age >= 18 ? <div>Over Age</div> : <div> under Age</div> }</div>
+    <div style={{color: age <= 18 ? "red" : "blue"}}>{age >= 18 ? <div>Over Age</div> : <div> under Age</div> }
+    <div>{age >= 18 ? <button> this is a button</button> : <h1>15</h1>}</div>
+    {listofname.map((element, value) => {
+      return <div>{element} : {value}</div>
+    })}
+    </div>
+
     // <div className="App">
     //   <User name="achraf" age={23} email="achraf@achraf.com"/>
     //   <User name="test" age={2665} email="jake@achraf.com"/>
