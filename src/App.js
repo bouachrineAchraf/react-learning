05a984c77d4e8da6b9a2e2a5f5d18ef86d33cfbc
bouachrine <bouachrineachraf04@gmail.com>
2023-12-18 16:26:32 +0100

    display a list of users with user component

--- src/App.js
+++ src/App.js
@@ -4,24 +4,25 @@ function App() {
   const age = 17;
   const listofname = ["achraf", "oum", "test"]
   const mapofname = [
-    {name : "achraf", age:21},
-    {name : "oum", age:22},
-    {name : "aadk", age:23},
-
-
-
+    { name: "achraf", age: 21 },
+    { name: "oum", age: 22 },
+    { name: "aadk", age: 23 },
   ]
 
   return (
-    <div style={{color: age <= 18 ? "red" : "blue"}}>{age >= 18 ? <div>Over Age</div> : <div> under Age</div> }
-    <div>{age >= 18 ? <button> this is a button</button> : <h1>15</h1>}</div>
-    {listofname.map((element, value) => {
-      return <div>{element} : {value}</div>
-    })}
+    <div style={{ color: age <= 18 ? "red" : "blue" }}>{age >= 18 ? <div>Over Age</div> : <div> under Age</div>}
+      <div>{age >= 18 ? <button> this is a button</button> : <h1>15</h1>}</div>
+      {listofname.map((element, value) => {
+        return <div>{element} : {value}</div>
+      })}
 
-    {mapofname.map((user, index) => {
-      return <div>{user.name} | {index}</div>
-    })}
+      {mapofname.map((user, index) => {
+        return <div>{user.name} | {index}</div>
+      })}
+
+      {mapofname.map((user, index) => {
+        return <User name={user.name} age={user.age}></User>
+      })}
     </div>
 
     // <div className="App">
