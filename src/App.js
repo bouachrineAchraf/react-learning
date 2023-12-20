80ef471bc80df8094294d3ff105f5e9895a8cf1d
bouachrine <bouachrineachraf04@gmail.com>
2023-12-20 11:12:23 +0100

    useState show and hide function

--- src/App.js
+++ src/App.js
@@ -4,12 +4,19 @@ import {Planet} from './planet'
 import {useState} from "react"
 
 function App() {
+ const [state, setState] = useState(true);
+ const changeState = () => {
+  setState(!state)
+ }
  const [input, setInput] = useState("")
  const changeText = (event) => {
   setInput(event.target.value)
  } 
  return (
     <div>
+      <button onClick={changeState}>click here</button>
+      <div>{state && <div>hey text</div>}</div>
+
       <input type="text" onChange={changeText}/>
       {input}
     </div>
