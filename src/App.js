410c404484bd6b4b0fd202962919d2b061222c24
bouachrine <bouachrineachraf04@gmail.com>
2023-12-20 16:43:38 +0100

    delete element basic

--- src/App.js
+++ src/App.js
@@ -1,29 +1,39 @@
 import './App.css';
-import {User} from "./user"
-import {Planet} from './planet'
-import {useState} from "react"
+import { User } from "./user"
+import { Planet } from './planet'
+import { useState } from "react"
 
 function App() {
- const [list, setList] = useState([]);
-const [task, setTask] = useState("")
- const addTast = () => {
+  const [list, setList] = useState([]);
+  const [task, setTask] = useState("")
+  const addTast = () => {
   const newlist = [...list, task]
-  setList(newlist);
+    setList(newlist);
+  }
 
- }
+  const handleTask = (event) => {
+    setTask(event.target.value)
+  }
 
- const handleTask = (event) => {
-  setTask(event.target.value)  
-
- }
+  const deleteElement = (element) => {
+    const arr = list.filter((name) => {
+      if (name == element) {
+        return false
+      }
+      else return true 
+    })
+    setList(arr)
+  }
   return (
-  <div>
-    <input onChange={handleTask}/>
-    <button onClick={addTast}>Add task</button>
-    {list}
-  </div>
+    <div>
+      <input onChange={handleTask} />
+      <button onClick={addTast}>Add task</button>
+      {list.map((element) => {
+        return <div>{element} <button onClick={() => deleteElement(element)}>X</button></div>
+      })}
+    </div>
   )
-   
+
 }
 
 
