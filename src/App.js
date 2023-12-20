adb83936b076ed530ba635ebc2c01a07f7df608b
bouachrine <bouachrineachraf04@gmail.com>
2023-12-20 16:35:39 +0100

    crud: add element

--- src/App.js
+++ src/App.js
@@ -4,9 +4,24 @@ import {Planet} from './planet'
 import {useState} from "react"
 
 function App() {
- 
- return (
-  <div></div>
+ const [list, setList] = useState([]);
+const [task, setTask] = useState("")
+ const addTast = () => {
+  const newlist = [...list, task]
+  setList(newlist);
+
+ }
+
+ const handleTask = (event) => {
+  setTask(event.target.value)  
+
+ }
+  return (
+  <div>
+    <input onChange={handleTask}/>
+    <button onClick={addTast}>Add task</button>
+    {list}
+  </div>
   )
    
 }
