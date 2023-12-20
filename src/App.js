8262894b92460de68eac7fbf39e51a35d71af837
bouachrine <bouachrineachraf04@gmail.com>
2023-12-20 16:13:43 +0100

    la base

--- src/App.js
+++ src/App.js
@@ -4,26 +4,9 @@ import {Planet} from './planet'
 import {useState} from "react"
 
 function App() {
- const [num, setNum] = useState(0);
-
- const Increase = () => {
-  setNum(num+1)
- }
  
- const Decrease = () => {
-  setNum(num-1)
- }
-
- const SetToZero = () => {
-  setNum(0)
- }
  return (
-    <div>
-      <button onClick={Increase}>Increase</button>
-      <button onClick={Decrease}>Decrease</button>
-      <button onClick={SetToZero}>Set To Zero</button>
-      {num}
-    </div>
+  <div></div>
   )
    
 }
