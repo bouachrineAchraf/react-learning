cebf161f115b5ba0f499f3bf6aae2ed4242a7725
bouachrine <bouachrineachraf04@gmail.com>
2023-12-21 15:40:15 +0100

    fetch with a fonction

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import {useEffect, useState} from "react"
 import Axios from "axios"
 
 function App() {
+  const [fact, setFact] = useState("")
+  const fetch = () => {
+   Axios.get("https://catfact.ninja/fact").then((res) => setFact(res.data.fact))
+  }
  useEffect(() => {
   console.log("componsant mounted");
  }, [])
@@ -12,7 +16,10 @@ function App() {
  //fetch("https://catfact.ninja/fact").then((data) => data.json()).then((data) => console.log(data))
  Axios.get("https://catfact.ninja/fact").then((res) => console.log("axios object : ", res.data))
  return (
-  <div> i am APP component</div>
+  <div> 
+    <button onClick={fetch}>Generate Cat Fact</button>
+    <div>{fact}</div>
+  </div>
   )
    
 }
