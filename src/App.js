8b225cd486e4ed1ab84ca4b1ed3ef1726370f466
bouachrine <bouachrineachraf04@gmail.com>
2023-12-21 15:34:36 +0100

    fetching data using axios

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import {User} from "./user"
 import {Planet} from './planet'
 import {useEffect, useState} from "react"
+import Axios from "axios"
 
 function App() {
  useEffect(() => {
   console.log("componsant mounted");
  }, [])
 
- fetch("https://catfact.ninja/fact").then((data) => data.json()).then((data) => console.log(data))
+ //fetch("https://catfact.ninja/fact").then((data) => data.json()).then((data) => console.log(data))
+ Axios.get("https://catfact.ninja/fact").then((res) => console.log("axios object : ", res.data))
  return (
   <div> i am APP component</div>
   )
