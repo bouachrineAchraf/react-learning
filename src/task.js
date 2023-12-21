2d644b6f28ec24aeb361b9a23ea8db2bb84a5014
bouachrine <bouachrineachraf04@gmail.com>
2023-12-21 11:47:57 +0100

    crud: change background color

--- src/task.js
+++ src/task.js
@@ -1,15 +1,15 @@
 import React, { Component, useState } from 'react';
 
 export const Task = (props) => {
-    const [color, setColor] = useState("red")
+    const [color, setColor] = useState("white")
 
     const changeColor = () => {
         setColor("green")
     }
   return (
     <div>
-      <div>
-        <span style={{ color: color }}>{props.element.name}</span>
+      <div style={{ backgroundColor: color }}>
+        <span >{props.element.name}</span>
         <button onClick={() => props.delete(props.element.id)}>X</button>
         <div></div>
         <button onClick={() => changeColor()}>complete</button>
