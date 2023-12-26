585521c4d5e82703f356d56e6a1eee32395e3587
bouachrine <bouachrineachraf04@gmail.com>
2023-12-26 15:23:52 +0100

    useQuery :isLoading, IsError, refetch options

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,21 @@ import { useQuery } from "@tanstack/react-query";
 import Axios from "axios";
 
 export const Home = () => {
-  const { data } = useQuery({
+  const { data: catFact, isLoading, isError, refetch } = useQuery({
     queryKey: ["cat"],
     queryFn: () => {
       return Axios.get("https://catfact.ninja/fact").then((res) => res.data);
     },
   });
 
-  return <div>this is the home page</div>;
+  if (isError) {
+    return <h1>there is an Error here</h1>
+  }
+  if (isLoading) {
+    return <h1>Its Loading ...</h1>
+  }
+
+  return <div>this is the home page<p>{catFact?.fact}</p>
+  
+  <div><button onClick={refetch}>update </button></div></div>;
 };
