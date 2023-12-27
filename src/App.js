65108722ce297bb431c7eb00bb24f283a4a8f2df
bouachrine <bouachrineachraf04@gmail.com>
2023-12-27 11:34:36 +0100

    is visible hooks

--- src/App.js
+++ src/App.js
@@ -1,34 +1,13 @@
 import './App.css';
-import { User } from "./user"
-import { Planet } from './planet'
-import { useEffect, useState, createContext } from "react"
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import { Home } from "./pages/Home"
-import { Contact } from "./pages/Contact"
-import { Menu } from "./pages/Menu"
-import { Navbar } from './pages/Navbar';
-import { Profile } from './pages/profile';
-import { SetProfile } from './pages/SetProfile';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useEffect, useState} from "react"
 
-export const AppContect = createContext()
 
 function App() {
-  const client = new QueryClient();
+  const [isVisible, setIsVisible] = useState(true);
   return (
     <div >
-      <QueryClientProvider client={client}>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/menu" element={<Menu />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="*" element={<h1>Page Not Found</h1>} />
-        </Routes>
-      </Router>
-      </QueryClientProvider>
+      <button onClick={() => {setIsVisible((value) => !value)}}>{isVisible ? "Hide" : "Show"}</button>
+      <p>{isVisible && <h1>Hi Achraf .</h1>}</p>
     </div>
   )
 
--- src/pages/SetProfile.js
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react"
-import { AppContect } from "../App"
-export const SetProfile = (props) => {
-    const {setProfile} = useContext(AppContect)
-
-    return(
-        <div><input onChange={(event) => setProfile(event.target.value)}/></div>
-    )
-}
